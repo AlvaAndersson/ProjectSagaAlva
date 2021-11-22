e46be1454fa76b44a5c8c50130c89696944f9050
04ALANB <04ALANB@LIDT-S4X33606>
2021-11-22 15:59:29 +0100

    Add project files.

---
+++ ProjectSagaAlva/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace ProjectSagaAlva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int antalLåtar = 20;
+
+            string[] Lista = new string[antalLåtar];
+
+            Console.WriteLine($"Välkommen till musiktoppistan! Skirv in {antalLåtar} låtar.");
+            string menyVal = " ";
+
+            while(menyVal != "4")
+            {
+                Console.WriteLine("1. Visa hela topplistan");
+                Console.WriteLine("2. Ändra låt");
+                Console.WriteLine("3. sök efter ord");
+                Console.WriteLine("4. Avsluta");
+
+                switch (menyVal)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Programmet kommer att avslutas...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Du valde inget giltigt alnternativ");
+                        break;
+
+
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
